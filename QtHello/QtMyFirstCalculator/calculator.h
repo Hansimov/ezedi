37db8d85616ef78b14ad9bd5ffdde9d81c710ecd
asimov <591172499@qq.com>
2019-10-07 22:49:41 +0800

    + Add QtMyFirstCalculator

---
+++ QtHello/QtMyFirstCalculator/calculator.h
@@ -0,0 +1,16 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+class QLineEdit;
+QT_END_NAMESPACE
+class Button;
+
+class Calcutor : public QWidget
+{
+    Q_OBJECT
+};
+
+#endif // CALCULATOR_H
